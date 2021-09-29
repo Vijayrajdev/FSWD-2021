ffb43b561067fbbff174e249d4b361ded4529db7
Vijayraj-wd <vijayrajck@outlook.com>
2021-09-29 19:32:23 +0530

    class and Id added

--- assets/Javascript/index.js
+++ assets/Javascript/index.js
@@ -1,2 +1,11 @@
-console.log("vijay");
-console.warn("vj");
+var heading = document.getElementById("head");
+var para = document.getElementsByClassName("para");
+var tags = document.getElementsByTagName("a");
+var tag = document.querySelector(".para");
+
+console.log(para);
+console.log(tag);
+console.log(tags);
+console.log(heading);
+
+heading.innerHTML = "Hi from JS";
