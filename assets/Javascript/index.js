e407e106d747f46921c63488028afcfbeac1fb92
Vijayraj-wd <vijayrajck@outlook.com>
2021-09-29 19:47:30 +0530

    classlist

--- assets/Javascript/index.js
+++ assets/Javascript/index.js
@@ -1,11 +1,23 @@
-var heading = document.getElementById("head");
-var para = document.getElementsByClassName("para");
-var tags = document.getElementsByTagName("a");
-var tag = document.querySelector(".para");
+// var heading = document.getElementById("head");
+// var para = document.getElementsByClassName("para");
+// var tags = document.getElementsByTagName("a");
+// var tag = document.querySelector(".para");
+//
+// console.log(para);
+// console.log(tag);
+// console.log(tags);
+// console.log(heading);
+//
+// heading.innerHTML = "Hi from JS";
 
-console.log(para);
-console.log(tag);
-console.log(tags);
-console.log(heading);
+var list = document.querySelector(".list");
 
-heading.innerHTML = "Hi from JS";
+var arr = ["one", "two", "three"];
+arr.push("four");
+
+for (var i = 0; i < arr.length; i++) {
+  var element = document.createElement("li");
+  var textNode = document.createTextNode(arr[i]);
+  element.appendChild(textNode);
+  list.appendChild(element);
+}
