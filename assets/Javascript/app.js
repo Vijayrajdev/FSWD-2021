942ba1ab53a43351596b96395f5a181c79bd6033
Vijayraj-wd <vijayrajck@outlook.com>
2021-10-01 12:12:57 +0530

    dark mode

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,12 +1,22 @@
+// const button = document.querySelector("button");
+// const input = document.querySelector("input");
+// const list = document.querySelector("ul");
+//
+// button.addEventListener("click", (e) => {
+//   const inputValue = input.value;
+//   const element = document.createElement("li");
+//   const textNode = document.createTextNode(inputValue);
+//
+//   element.appendChild(textNode);
+//   list.appendChild(element);
+// });
+
+const body = document.querySelector("body");
 const button = document.querySelector("button");
-const input = document.querySelector("input");
-const list = document.querySelector("ul");
 
-button.addEventListener("click", (e) => {
-  const inputValue = input.value;
-  const element = document.createElement("li");
-  const textNode = document.createTextNode(inputValue);
-
-  element.appendChild(textNode);
-  list.appendChild(element);
-});
+button.addEventListener(
+  "click",
+  (mode = (e) => {
+    body.classList.toggle("dark");
+  })
+);
