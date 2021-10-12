04cd35fe8738a4aca56111023cb41317a85e3d2f
Vijayraj-wd <vijayrajck@outlook.com>
2021-10-12 11:14:27 +0530

    local storage

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const stuff = require('./utils');
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+const div = document.querySelector("div");
 
-stuff.add()
-stuff.subract()
-stuff.multiply()
-stuff.divide()
+const arr = [1, 2, 3, 4, 5];
+
+localStorage.setItem("Inputvalue", JSON.stringify(arr));
+
+console.log(JSON.parse(localStorage.getItem("Inputvalue")));
