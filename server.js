ed002a761ba9b61f51e7309d75f6bceabe456232
Vijayraj-wd <vijayrajck@outlook.com>
2021-10-12 19:48:16 +0530

    express

--- server.js
+++ server.js
@@ -1,1 +1,47 @@
-console.log('A message');
+// const http = require('http')
+// const fs = require('fs')
+// const path = require('path')
+
+// const server = http.createServer((request, response) => {
+
+//     const { url } = request
+//     if (url == '/login') {
+//         response.write(`<h1>Login</h1>`)
+//         response.end()
+//     }
+//     else if (url == '/signup') {
+//         response.write(`<h1>signup</h1>`)
+//         response.end()
+//     }
+//     else {
+//         response.write(`<h1>Error</h1>
+//       <button><a href="http://localhost:3000/login">Click</a></button>`)
+//         response.end()
+//     }
+// })
+
+// const port = 3000;
+// server.listen(port, () => {
+//     console.log(`server is up and running in port ${port} ...`)
+// })
+
+const express = require('express')
+const app = express()
+
+app.get('/login', (req, res) => {
+    res.send(`<h1>Login</h1>`)
+})
+
+app.get('/signup', (req, res) => {
+    res.send(`<h1>signup</h1>`)
+})
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Error</h1>
+       <button><a href="http://localhost:3000/login">Click</a></button>`)
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is up and running in port ${port}`)
+})
